8ba669d313d455d97ebedcdc4d72a10abf31b8e7
Nicola Meoli <46779155+sononicola@users.noreply.github.com>
2022-03-07 14:53:07 +0100

    same

--- maxs_mins.py
+++ maxs_mins.py
@@ -1,4 +1,5 @@
 import numpy as np
+from global_variables import *
 
 def find_max_xy_all(x: list, y:list) -> list[tuple]:
     """
@@ -45,7 +46,8 @@ def find_min_xy(x: list, y:list) -> tuple:
     index = np.argmin(y)
     return x[index], y[index]
 
-def find_local_max_xy(x: np.array, y:np.array, a:float, b:float, tol:float) -> tuple: #TODO sistemare il typing
+def find_local_max_xy(x: np.array, y:np.array, a:float, b:float) -> tuple: #TODO sistemare il typing
+    tol = ARANGE_STEP
     """
     Find the local max y value of and x,y plot and the associated x, corresponding to limits: x==a and x==b 
     Returns index, x(index), y(index) 
@@ -61,11 +63,14 @@ def find_local_max_xy(x: np.array, y:np.array, a:float, b:float, tol:float) -> t
     """
     a_index = np.where((x >= a - tol) & (x <= a + tol))[0][0]
     b_index = np.where((x >= b - tol) & (x <= b + tol))[0][0]
+    #a_index = np.where((x >= a) & (x <= a + 20*tol))[0][0]
+    #b_index = np.where((x >= b - 20*tol) & (x <= b))[0][0]
     index = np.argmax(y[a_index:b_index+1])     # this is the index of the sliced y, not of the original y array
     index = a_index + index                     # so we need to add it to the index of the initial value a
     return index, x[index], y[index]    # so we need to add it to the index of the initial value a
 
-def find_local_min_xy(x: np.array, y:np.array, a:float, b:float, tol:float) -> tuple:
+def find_local_min_xy(x: np.array, y:np.array, a:float, b:float) -> tuple:
+    tol = ARANGE_STEP
     """
     Find the local min y value of and x,y plot and the associated x, corresponding to limits: x==a and x==b.
     Returns index, x(index), y(index) 
@@ -83,7 +88,8 @@ def find_local_min_xy(x: np.array, y:np.array, a:float, b:float, tol:float) -> t
     index = np.argmin(y[a_index:b_index+1])     # this is the index of the sliced y, not of the original y array
     index = a_index + index                     # so we need to add it to the index of the initial value a
     return index, x[index], y[index]  
-def list_of_max_indexes(x: np.array, y:np.array, list_of_points: list, tol:float) -> list:
+def list_of_max_indexes(x: np.array, y:np.array, list_of_points: list) -> list:
+    tol = ARANGE_STEP
 
     # Due to find_local_max_xy's tollerance value, we need to extend the lists because the last boundary exit from x list when checking np.where(b+tol)
     list_of_points = np.append(list_of_points, list_of_points[-1] + tol)
@@ -91,17 +97,18 @@ def list_of_max_indexes(x: np.array, y:np.array, list_of_points: list, tol:float
     y = np.append(y, y[-1])
 
     # Maximum at supports:
-    list_of_indexes_supports = [find_local_max_xy(x, y, list_of_points[i] , list_of_points[i], tol=tol)[0] for i in range(len(list_of_points)-1)]
+    list_of_indexes_supports = [find_local_max_xy(x, y, list_of_points[i] , list_of_points[i])[0] for i in range(len(list_of_points)-1)]
     list_of_indexes_supports[-1] = list_of_indexes_supports[-1] -1  # last index corrisponds of total lenght in arange, but it's out of boundary for x and y 
     # Maximum in the middle of spans: (len -2 because lists are n+1)
-    list_of_indexes_spans = [find_local_max_xy(x, y, list_of_points[i] , list_of_points[i+1], tol=tol)[0] for i in range(0,len(list_of_points)-2)]
+    list_of_indexes_spans = [find_local_max_xy(x, y, list_of_points[i] , list_of_points[i+1])[0] for i in range(0,len(list_of_points)-2)]
 
     list_of_indexes_all = list_of_indexes_supports + list_of_indexes_spans 
     list_of_indexes_all.sort()
 
     return list_of_indexes_all
 
-def list_of_min_indexes(x: np.array, y:np.array, list_of_points: list, tol:float) -> list:
+def list_of_min_indexes(x: np.array, y:np.array, list_of_points: list) -> list:
+    tol = ARANGE_STEP
 
     # Due to find_local_min_xy's tollerance value, we need to extend the lists because the last boundary exit from x list when checking np.where(b+tol)
     list_of_points = np.append(list_of_points, list_of_points[-1] + tol)
@@ -109,11 +116,11 @@ def list_of_min_indexes(x: np.array, y:np.array, list_of_points: list, tol:float
     y = np.append(y, y[-1])
 
     # Minimum at supports:
-    list_of_indexes_supports = [find_local_min_xy(x, y, list_of_points[i] , list_of_points[i], tol=tol)[0] for i in range(len(list_of_points)-1)]
+    list_of_indexes_supports = [find_local_min_xy(x, y, list_of_points[i] , list_of_points[i])[0] for i in range(len(list_of_points)-1)]
     list_of_indexes_supports[-1] = list_of_indexes_supports[-1] -1  # last index corrisponds of total lenght in arange, but it's out of boundary for x and y 
 
     # Minimum in the middle of spans: (len -2 because lists are n+1)
-    list_of_indexes_spans = [find_local_min_xy(x, y, list_of_points[i] , list_of_points[i+1], tol=tol)[0] for i in range(0,len(list_of_points)-2)]
+    list_of_indexes_spans = [find_local_min_xy(x, y, list_of_points[i] , list_of_points[i+1])[0] for i in range(0,len(list_of_points)-2)]
 
     list_of_indexes_all = list_of_indexes_supports + list_of_indexes_spans 
     list_of_indexes_all.sort()
