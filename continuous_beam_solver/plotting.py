c0df68dd586550083147f62e76cf0b41fe366b26
Nicola Meoli <46779155+sononicola@users.noreply.github.com>
2022-03-10 09:57:13 +0100

    prova science su streamlit cloud

--- continuous_beam_solver/plotting.py
+++ continuous_beam_solver/plotting.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+plt.style.use('science')
+plt.rcParams['text.usetex'] = True
 
 #plt.rcParams['text.usetex'] = True
 class Plot:
@@ -7,19 +9,19 @@ class Plot:
         """This is just a style definitions for plot methods. 
         list_of_y_points can be an array of y points or a list of arrays of y points (like in the inviluppo methods)
         """
-        fig, ax = plt.subplots(1,1, figsize = (10, 5), tight_layout=True)
+        fig, ax = plt.subplots(1,1, figsize = (16, 10), tight_layout=True)
+       
         ax.invert_yaxis()
         ax.set_xlim(x_thicks[0], x_thicks[-1])
         ax.set_ylim(np.max(list_of_y_points), np.min(list_of_y_points) ) # invertiti perché l'asse è invertito
         ax.grid("True")
-        ax.axhline(0, color='grey', linewidth=2)
+        ax.axhline(0)
         #ax.vlines(cum_lenghts, ymin=y_min, ymax=y_max,linestyles='dotted')
         #ax.set_xticklabels(cum_lenghts) # per il nome campata magari
-        ax.set_xticks(x_thicks) #TODO aggiungere xthick nel massimo in mezzeria
+        ax.set_xticks(np.round(x_thicks,2)) #TODO aggiungere xthick nel massimo in mezzeria
         #ax.set_yticks(np.arange(-300, 250, step=50)) #TODO
-        ax.set_title(title, fontsize=20)
-        ax.set_xlabel(r"L")
-        ax.set_ylabel(y_label) #TODO aggiungere l'if se si usa per il taglio
+        ax.tick_params(axis='both', labelsize=12)
+        ax.set_title(title, fontsize=18)
         return  fig, ax
 
     def plot_y_points(s_func:list, y_points:list, title:str, x_thicks:list, y_label:str, color:str):
